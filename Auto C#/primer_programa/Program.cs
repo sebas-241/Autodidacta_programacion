9b96718886e5b97c45bc7e4f67d3da9a394b7d7b
Sebastian Gonzalez Rodriguez <sebgonrod@gmail.com>
2022-12-15 16:29:07 -0500

    Save

--- Auto C#/primer_programa/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Bienvenid@");
-Console.WriteLine("Escriba su nombre: ");
-string nombre = Console.ReadLine();
-Console.WriteLine("Escriba su edad: ");
-string edad = Console.ReadLine();
-Console.WriteLine("Hola" + nombre, "tiene" + edad + "años");
-
